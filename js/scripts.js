b1fa8e23ceabc56ab102c0e3e1699c34906c32eb
EJQuezada <equezada@msn.com>
2022-11-08 11:05:47 +0100

    Eliminated extra spacing in items list

--- js/scripts.js
+++ js/scripts.js
@@ -4,55 +4,46 @@ let pokemonList= [
         height: 0.4,
         type: ['electric']
     },
-
     {
         name: 'Charizard',
         height: 1.7,
         type: ['fire', 'flying']
     },
-
     {
         name: 'Squirtle',
         height: 0.5,
         type: ['water']
     },
-
     {
         name: 'Primeape',
         height: 1,
         type: ['fighting']
     },
-
     {
         name: 'Pidgeot',
         height: 1.5,
         type: ['flying', 'normal']
     },
-
     {
         name: 'Meowth',
         height: 0.4,
         type: ['normal']
     },
-
     {
         name: 'Ivysaur',
         height: 1.0,
         type: ['grass', 'poison']
     },
-
     {
         name: 'Blastoise',
         height: 1.6,
         type: ['water']
     },
-
     {
         name: 'Golbat',
         height: 1.6,
         type: ['poison', 'flying']
     },
-
     {
         name: 'Dugtrio',
         height: 0.7,
