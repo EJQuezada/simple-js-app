54614a60d33d5489141e3f5d408e56206b67f81d
EJQuezada <equezada@msn.com>
2022-11-11 00:43:49 +0100

    added space after document.write

--- js/scripts.js
+++ js/scripts.js
@@ -62,10 +62,10 @@ let pokemonList= [
 ];
 for (let i = 0; i < pokemonList.length; i++) {
         if (pokemonList[i].height > 1.6) {
-            document.write(pokemonList[i].name + pokemonList[i].height) + Wow, thats a big Pokemon!
+            document.write (pokemonList[i].name + pokemonList[i].height ) + Wow, thats a big Pokemon!
         }
         else {
-            document.write(pokemonList[i].name + pokemonList[i].height) 
+            document.write (pokemonList[i].name + pokemonList[i].height ) 
         }
 }
     
