82583f5a22b2fda6f3efba9344f67c4ffc7d03bb
EJQuezada <equezada@msn.com>
2022-11-11 10:59:39 +0100

    replaced document.write( ) with console.log( )

--- js/scripts.js
+++ js/scripts.js
@@ -62,10 +62,10 @@ let pokemonList = [
 ];
 for (let i = 0; i < pokemonList.length; i++) {
         if (pokemonList[i].height > 1.6) {
-            document.write (pokemonList[i].name  +  pokemonList[i].height  + 'Wow, thats a big Pokemon!' )
+            console.log(pokemonList[i].name  +  pokemonList[i].height  + 'Wow, thats a big Pokemon!' )
         }
         else {
-            document.write (pokemonList[i].name + pokemonList[i].height ) 
+            console.log(pokemonList[i].name + pokemonList[i].height ) 
         }
 }
     
