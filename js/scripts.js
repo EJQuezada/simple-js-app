e02587f50a59421a5a4156108e651cfdd574831a
EJQuezada <115997231+EJQuezada@users.noreply.github.com>
2022-11-17 12:05:29 +0100

    adjust tabbing  to match main branch

--- js/scripts.js
+++ js/scripts.js
@@ -1,84 +1,84 @@
 let pokemonRepository= (function() {
     let pokemonList= [
-        {
-            name: 'Pikachu',
-            height: 0.4,
-            weight: 6,
-            type: ['electric']
-        }, 
-        {
-            name: 'Charizard',
-            height: '1.7',
-            weight: 90.5,
-            type: ['fire', 'flying']
-        }, 
-        {
-            name: 'Squirtle',
-            height: 0.5,
-            weight: 9,
-            type: ['water']
-        }, 
-        {
-            name: 'Primeape',
-            height: 1,
-            weight: 32,
-            type: ['fighting']
-        }, 
-        {
-            name: 'Pidgeot',
-            height: 1.5,
-            weight: 39.5,
-            type: ['flying', 'normal']
-        }, 
-        {
-            name: 'Meowth',
-            height: 0.4,
-            weight: 4.2,
-            type: ['normal']
-        }, 
-        {
-            name: 'Ivysaur',
-            height: 1.0,
-            weight: 13,
-            type: ['grass', 'poison']
-        }, 
-        {
-            name: 'Blastoise',
-            height: 1.6,
-            weight: 85.5,
-            type: ['water']
-        }, 
-        {
-            name: 'Golbat',
-            height: 1.6,
-            weight: 55,
-            type: ['poison', 'flying']
-        }, 
-        {
-            name: 'Dugtrio',
-            height: 0.7,
-            weight: 33.3,
-            type: ['ground']
-        }    
-    ];
+    {
+        name: 'Pikachu',
+        height: 0.4,
+        weight: 6,
+        type: ['electric']
+    }, 
+    {
+        name: 'Charizard',
+        height: '1.7',
+        weight: 90.5,
+        type: ['fire', 'flying']
+    }, 
+    {
+        name: 'Squirtle',
+        height: 0.5,
+        weight: 9,
+        type: ['water']
+    }, 
+    {
+        name: 'Primeape',
+        height: 1,
+        weight: 32,
+        type: ['fighting']
+    }, 
+    {
+        name: 'Pidgeot',
+        height: 1.5,
+        weight: 39.5,
+        type: ['flying', 'normal']
+    }, 
+    {
+        name: 'Meowth',
+        height: 0.4,
+        weight: 4.2,
+        type: ['normal']
+    }, 
+    {
+        name: 'Ivysaur',
+        height: 1.0,
+        weight: 13,
+        type: ['grass', 'poison']
+    }, 
+    {
+        name: 'Blastoise',
+        height: 1.6,
+        weight: 85.5,
+        type: ['water']
+    }, 
+    {
+        name: 'Golbat',
+        height: 1.6,
+        weight: 55,
+        type: ['poison', 'flying']
+    }, 
+    {
+        name: 'Dugtrio',
+        height: 0.7,
+        weight: 33.3,
+        type: ['ground']
+    }    
+];
 
-        function add(pokemon) {
-            pokemonList.push(pokemon);
-        }
-        function getAll() {
-            return pokemonList;
-        }
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+    function getAll() {
+        return pokemonList;
+    }
 
-        return {
-            getAll: getAll,
-            add: add
-        };
+    return {
+        getAll: getAll,
+        add: add
+    };
 /*pokemonList.forEach(function(_pokemon) {
-        if (_pokemon.height >= 1.7) {
-            document.write(_pokemon.name  +  _pokemon.height + ' - Wow, thats big!' + "</br>"  )
-        }
-        else {
-            document.write(_pokemon.name  +  _pokemon.height + "</br>"  )
-        }
+    if (_pokemon.height >= 1.7) {
+        document.write(_pokemon.name  +  _pokemon.height + ' - Wow, thats big!' + "</br>"  )
+    }
+    else {
+        document.write(_pokemon.name  +  _pokemon.height + "</br>"  )
+    }
 */        
 })();
