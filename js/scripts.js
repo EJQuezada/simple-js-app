1689a96578d092c056fd38e5d6abfb25e0df270f
EJQuezada <equezada@msn.com>
2022-11-18 11:13:17 +0100

    add commas after each list item and add functions

--- js/scripts.js
+++ js/scripts.js
@@ -4,74 +4,74 @@ let pokemonRepository = (function () {
         name: 'Pikachu',
         height: 0.4,
         weight: 6,
-        type: ['electric']
+        type: ['electric'],
     }, 
     {
         name: 'Charizard',
         height: '1.7',
         weight: 90.5,
-        type: ['fire', 'flying']
+        type: ['fire', 'flying'],
     }, 
     {
         name: 'Squirtle',
         height: 0.5,
         weight: 9,
-        type: ['water']
+        type: ['water'],
     }, 
     {
         name: 'Primeape',
         height: 1,
         weight: 32,
-        type: ['fighting']
+        type: ['fighting'],
     }, 
     {
         name: 'Pidgeot',
         height: 1.5,
         weight: 39.5,
-        type: ['flying', 'normal']
+        type: ['flying', 'normal'],
     }, 
     {
         name: 'Meowth',
         height: 0.4,
         weight: 4.2,
-        type: ['normal']
+        type: ['normal'],
     }, 
     {
         name: 'Ivysaur',
         height: 1.0,
         weight: 13,
-        type: ['grass', 'poison']
+        type: ['grass', 'poison'],
     }, 
     {
         name: 'Blastoise',
         height: 1.6,
         weight: 85.5,
-        type: ['water']
+        type: ['water'],
     }, 
     {
         name: 'Golbat',
         height: 1.6,
         weight: 55,
-        type: ['poison', 'flying']
+        type: ['poison', 'flying'],
     }, 
     {
         name: 'Dugtrio',
         height: 0.7,
         weight: 33.3,
-        type: ['ground']
-    }    
+        type: ['ground'],
+    },    
 ];
 
     function add(pokemon) {
         pokemonList.push(pokemon);
-    },
+    }
     function getAll() {
         return pokemonList;
     }
 
     return {
         getAll: getAll,
-        add: add
+        add: add,
     };
 /*pokemonList.forEach(function(_pokemon) {
     if (_pokemon.height >= 1.7) {
@@ -83,3 +83,11 @@ let pokemonRepository = (function () {
 */
 })();
     
+pokemonRepository.getAll().forEach(function (pokemon) {
+    if (pokemon.height >= 1.7) {
+        document.write(pokemon.name + pokemon.height + "- Wow, that's big!" + "</br>");
+    } 
+    else {
+        document.write(pokemon.name + pokemon.height + "</br>");
+    }
+});
