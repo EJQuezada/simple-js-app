0087e20c9124d5c4b9f0fba94dd4b88b7c3a4e78
EJQuezada <equezada@msn.com>
2022-11-22 15:05:29 +0100

    update "index.html" and "Scripts.js" for DOM inter

--- js/scripts.js
+++ js/scripts.js
@@ -84,10 +84,13 @@ let pokemonRepository = (function () {
 })();
     
 pokemonRepository.getAll().forEach(function (pokemon) {
-    if (pokemon.height >= 1.7) {
-        document.write(pokemon.name  +  pokemon.height +  "- Wow, that's big!" + "</br>");
-    } 
-    else {
-        document.write(pokemon.name  +  pokemon.height + "</br>");
-    }
+    let pokemonList = document.querySelector
+    (".pokemonList");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = "placeholder";
+    button.classList.add("button-class");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+
 });
