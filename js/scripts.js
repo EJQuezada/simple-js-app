34b3d6b1bc7c49cad3487cf964ccaa44c31366ce
EJQuezada <equezada@msn.com>
2022-11-28 17:06:33 +0100

    add functions inside repo to load external source

--- js/scripts.js
+++ js/scripts.js
@@ -96,11 +96,51 @@
 })();
 */
 let pokemonRepository = (function () {
-    let pokemonList = []
+    let pokemonList = [];
+    let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
 
-let pokemonRepository = (function () {
-    let pokemonList = [
+    function getAll() {
+        return pokemonRepository;
+    }  
+    
+    function addListItem(pokemon){
+        let pokemonList = document.querySelector
+        (".pokemonList");
+        let listpokemon = document.createElement("li");
+        let button = document.createElement("button");
+        button.innerText = pokemon.name;
+        button.classList.add("button-class");
+        listpokemon.appendChild(button);
+        pokemonList.appendChild(listpokemon);
+    }
+    
+    function loadList() {
+        return fetch(apiUrl).then(function (response) {
+            return response.json();
+        }).then (function (json) {
+            json.results.forEach(function (item) {
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+            });
+        }).catch(function (e) {
+            console.error(e);
+        })
+    }
 
-pokemonRepository.getAll().forEach(function (pokemon) {
-    pokemonRepository.addListItem(pokemon)
-});   
+    return {
+        getAll: getAll,
+        add: add,
+        addListItem: addListItem,
+        loadList: loadList
+    };
+    
+    pokemonRepository.loadList().then (function() {
+        pokemonRepository.getAll().forEach(function (pokemon) {
+                pokemonRepository.addListItem(pokemon);
+        });
+    });  
+
+})
