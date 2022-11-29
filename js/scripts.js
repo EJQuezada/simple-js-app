c0db64865a9b37e4003c78da424a586896abadac
EJQuezada <equezada@msn.com>
2022-11-29 09:32:52 +0100

    eliminate space after querySelector in PokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -116,8 +116,7 @@ let pokemonRepository = (function () {
     }
 
     function addListItem(pokemon){
-      let pokemonList = document.querySelector
-      (".pokemonList");
+      let pokemonList = document.querySelector(".pokemonList");
       let listpokemon = document.createElement("li");
       let button = document.createElement("button");
       button.innerText = pokemon.name;
