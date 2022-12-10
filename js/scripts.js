6b127c77fa328141e35c21fa96f90b94729d786c
EJQuezada <equezada@msn.com>
2022-12-10 17:17:53 +0100

    edit index.html to show bootstrap code for Modal

--- js/scripts.js
+++ js/scripts.js
@@ -100,10 +100,7 @@ let pokemonRepository = (function () {
     let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
 
     function add(pokemon) {
-        if (
-            typeof pokemon === "object" &&
-            "name" in pokemon && "detailsUrl" in pokemon           
-        ) {
+        if (typeof pokemon === "object" && "name" in pokemon && "detailsUrl" in pokemon) {
             pokemonList.push(pokemon);
         } else {
             console.log("pokemon is not correct");
@@ -113,14 +110,14 @@ let pokemonRepository = (function () {
         return pokemonList;
     }  
     
-    function addListItem(pokemon){
+    function addListItem(pokemon) {
         let pokemonList = document.querySelector(".pokemonList");
         let listpokemon = document.createElement("li");
         let button = document.createElement("button");
         button.innerText = pokemon.name;
         button.classList.add("button-class");
-        listpokemon.appendChild(button);
-        pokemonList.appendChild(listpokemon);
+        listItem.appendChild(button);
+        pokemonList.appendChild(listItem);
         button.addEventListener("click", function(event) {
             showDetails(pokemon);
         });
