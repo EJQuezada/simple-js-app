2cb2ff8d778499ca49ae4c33bc867e2ff4182d62
EJQuezada <equezada@msn.com>
2022-12-10 18:31:37 +0100

    add .pokemon-list element to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,62 +4,62 @@
         name: 'Pikachu',
         height: 0.4,
         weight: 6,
-        type: ['electric']
+        type: ['electric'],
     }, 
     {
         name: 'Charizard',
         height: '1.7',
         weight: 90.5,
-        type: ['fire', 'flying']
+        type: ['fire', 'flying'],
     }, 
     {
         name: 'Squirtle',
         height: 0.5,
         weight: 9,
-        type: ['water']
+        type: ['water'],
     }, 
     {
         name: 'Primeape',
         height: 1,
         weight: 32,
-        type: ['fighting']
+        type: ['fighting'],
     }, 
     {
         name: 'Pidgeot',
         height: 1.5,
         weight: 39.5,
-        type: ['flying', 'normal']
+        type: ['flying', 'normal'],
     }, 
     {
         name: 'Meowth',
         height: 0.4,
         weight: 4.2,
-        type: ['normal']
+        type: ['normal'],
     }, 
     {
         name: 'Ivysaur',
         height: 1.0,
         weight: 13,
-        type: ['grass', 'poison']
+        type: ['grass', 'poison'],
     }, 
     {
         name: 'Blastoise',
         height: 1.6,
         weight: 85.5,
-        type: ['water']
+        type: ['water'],
     }, 
     {
         name: 'Golbat',
         height: 1.6,
         weight: 55,
-        type: ['poison', 'flying']
+        type: ['poison', 'flying'],
     }, 
     {
         name: 'Dugtrio',
         height: 0.7,
         weight: 33.3,
-        type: ['ground']
-    }    
+        type: ['ground'],
+    },    
 ];
 
     function add(pokemon) {
@@ -79,25 +79,26 @@
         pokemonList.appendChild(listpokemon);
     
     }
-    
+
     return {
         getAll: getAll,
         add: add,
         addListItem: addListItem
     };
 /*pokemonList.forEach(function(_pokemon) {
-    if (_pokemon.height >= 1.7) {
+    if (pokemon.height >= 1.7) {
         document.write(_pokemon.name  +  _pokemon.height + ' - Wow, thats big!' + "</br>"  )
     }
     else {
         document.write(_pokemon.name  +  _pokemon.height + "</br>"  )
     }
-      
+
 })();
 */
 let pokemonRepository = (function () {
     let pokemonList = [];
     let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
+    let pokemonListElement = document.querySelector(".pokemon-list"); 
 
     function add(pokemon) {
         if (typeof pokemon === "object" && "name" in pokemon && "detailsUrl" in pokemon) {
@@ -108,21 +109,21 @@ let pokemonRepository = (function () {
     }
     function getAll() {
         return pokemonList;
-    }  
-    
+    }
+
     function addListItem(pokemon) {
-        let pokemonList = document.querySelector(".pokemonList");
+        let pokemonList = document.querySelector(".pokemon-list");
         let listItem = document.createElement("li");
         let button = document.createElement("button");
         button.innerText = pokemon.name;
         button.classList.add("button-class");
         listItem.appendChild(button);
-        pokemonList.appendChild(listItem);
+        pokemonList.appendChild(listItem);  
         button.addEventListener("click", function(event) {
             showDetails(pokemon);
         });
     }
-    
+
     function loadList() {
         return fetch(apiUrl).then(function (response) {
             return response.json();
@@ -208,7 +209,7 @@ let pokemonRepository = (function () {
             hideModal();
         }
     });
-    
+
     return {
         add: add,
         getAll: getAll,
@@ -220,9 +221,9 @@ let pokemonRepository = (function () {
         hideModal: hideModal
     };
 })();
-    
+
 pokemonRepository.loadList().then(function() {
     pokemonRepository.getAll().forEach(function (pokemon) {
         pokemonRepository.addListItem(pokemon);
     });
-});
+});   
