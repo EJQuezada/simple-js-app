943a967b710b89e58db83de464a7b1a45eea680c
EJQuezada <equezada@msn.com>
2022-12-10 18:35:13 +0100

    edit pokemonListElement to add .pokemon-list

--- js/scripts.js
+++ js/scripts.js
@@ -112,7 +112,7 @@ let pokemonRepository = (function () {
     }
 
     function addListItem(pokemon) {
-        let pokemonList = document.querySelector(".pokemonList");
+        let pokemonList = document.querySelector(".pokemon-list");
         let listItem = document.createElement("li");
         let button = document.createElement("button");
         button.innerText = pokemon.name;
