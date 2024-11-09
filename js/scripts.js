4c02e8adb6eae547617d6d715af081701df344b9
Edgar Quezada <equezada@msn.com>
2024-11-09 18:53:21 +0100

    Update scripts.js to increase pokedex from 150 to 999

--- js/scripts.js
+++ js/scripts.js
@@ -97,7 +97,7 @@
 */
 let pokemonRepository = (function () {
     let pokemonList = [];
-    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=999';
     let pokemonListElement = $('.pokemon-list'); 
 
     function add(pokemon) {
